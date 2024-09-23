bbe27ff1389edcbe9f4fb82f4f1ebadd671b9d0f
Andresito126 <creed126ax@gmail.com>
2024-09-23 02:48:30 -0600

    Add: The cover is almost ready, the body is ready

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomePComponent } from './home-p/home-p.component';
 import { AboutGameComponent } from './about-game/about-game.component';
-import { GameCoverComponent } from '../game-cover/game-cover.component';
+import { GameCoverComponent } from './game-cover/game-cover.component';
 
 
 
