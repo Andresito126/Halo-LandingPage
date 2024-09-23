2df70a556a74de48e01c5afa45748752941e5199
Andresito126 <creed126ax@gmail.com>
2024-09-22 18:27:27 -0600

    adding: Se agrego bullma. Se creo el esqueleto del la pagina web. Se creo el modulo home junto con el componente principal. Se realizo la ruta de home en router outlet y se mando a llamar al html de app.

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomePComponent } from './home-p/home-p.component';
+
+
+
+@NgModule({
+  declarations: [
+   
+  
+    HomePComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HomeModule { }
