e9365239a331bf3687e2182a5d6277fee08899ab
Andresito126 <creed126ax@gmail.com>
2024-09-23 03:09:52 -0600

    Add: making the component reusable whit the props

--- src/app/home/game-cover/game-cover.component.ts
+++ src/app/home/game-cover/game-cover.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-game-cover',
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class GameCoverComponent {
 
+  //estos seran los datos que se andaran reemplazando entre si, con ayuda del input 
+  @Input ()imgUrl: string= "";
+  @Input ()titleCover: string="";
+  @Input ()textCover: string="";
 }
