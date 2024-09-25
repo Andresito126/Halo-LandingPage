ac9e673477f2d715e734f95224a445a26513c17b
Andresito126 <creed126ax@gmail.com>
2024-09-24 18:15:15 -0600

    Add: exporting the caoroles form the module. Add:making the body about the history

--- src/app/history/history.module.ts
+++ src/app/history/history.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HistoryPComponent } from './history-p/history-p.component';
+import { HomeModule } from '../home/home.module';
 
 
 
@@ -9,7 +10,8 @@ import { HistoryPComponent } from './history-p/history-p.component';
     HistoryPComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    HomeModule
   ]
 })
 export class HistoryModule { }
